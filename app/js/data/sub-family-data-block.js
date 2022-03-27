6a1e87cf8f3a8fdf0b1b3400e9f5428bb12a10d4
Tim <nebukam@gmail.com>
2022-03-27 13:13:26 -0400

    Doc update

--- app/js/data/sub-family-data-block.js
+++ app/js/data/sub-family-data-block.js
@@ -55,13 +55,13 @@ class SubFamilyDataBlock extends SimpleDataEx {
             [IDS.WEIGHT_CLASS]: { value: ENUMS.WEIGHTS.At(3).value },
             [IDS.FONT_STYLE]: { value: `Regular` },
 
-            [IDS.CAP_HEIGHT]: { value: defaultEm * 0.8 },
-            [IDS.X_HEIGHT]: { value: defaultEm * 0.8 * 0.72 },
+            [IDS.CAP_HEIGHT]: { value: defaultEm * 0.7 },
+            [IDS.X_HEIGHT]: { value: defaultEm * 0.7 * 0.72 },
 
             [IDS.EM_UNITS]: { value: defaultEm },
             [IDS.EM_RESAMPLE]: { value: true },
             [IDS.BASELINE]: { value: defaultEm * 0.8 },
-            [IDS.ASCENT]: { value: defaultEm },
+            [IDS.ASCENT]: { value: defaultEm * 0.8 },
             [IDS.ASC_RESAMPLE]: { value: false },
             [IDS.DESCENT]: { value: defaultEm * -0.25 },
             //[IDS.H_ORIGIN_X]: { value: 0 },
